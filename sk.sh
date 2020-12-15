8e2c312f39c14dac0917230ae97ff4da8feb5928
developeranaz <71500526+developeranaz@users.noreply.github.com>
2020-12-15 14:19:26 +0530

    Update sk.sh

--- sk.sh
+++ sk.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 wget -nc 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
 unzip ngrok-stable-linux-amd64.zip config/* 2> /dev/null || true 
-./ngrok authtoken 1lVIHM7VN4hIrNnKxdaZHnmgL4y_7J8BhmSDj3WpHPjpNc7Pu
+#echo -n "AuthToken"
+./ngrok authtoken xxxxxxxxxxxxxxxxxx
 ./ngrok http 8080
