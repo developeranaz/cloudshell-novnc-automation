be9d0619da2698e08e65d8f8ba799e3b9f029cf4
developeranaz <71500526+developeranaz@users.noreply.github.com>
2020-12-16 12:32:03 +0530

    sh.sh
    novnc+docker+screen

--- sh.sh
+++ sh.sh
@@ -4,18 +4,6 @@
 #orgin-repository : https://github.com/developeranaz/cloudshell-novnc-automation
 #updating system
 sudo apt update
-#making ngrok directory 
-mkdir ngrok
-#changing directory to ngrok
-cd ngrok
-#removing all existing files 
-rm * 
-#removing all existing folders 
-rm -r *
-#downloading ngrok stable from official webserver
-wget 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
-#unzipping ngrok-stable-linux-amd64.zip in selected folder
-unzip ngrok-stable-linux-amd64.zip
 #installing screen
 sudo apt-get install screen
 #Activating screen
