279c4510ca0fafc02c4005af7678fc32794da375
developeranaz <71500526+developeranaz@users.noreply.github.com>
2020-12-16 13:04:42 +0530

    sk.sh
    ty

--- sk.sh
+++ sk.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#wget -nc 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
+#wget 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
 #unzip ngrok-stable-linux-amd64.zip config/* 2> /dev/null || true 
 #making ngrok directory 
 mkdir ngrok
@@ -14,5 +14,5 @@ wget 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
 #unzipping ngrok-stable-linux-amd64.zip in selected folder
 unzip ngrok-stable-linux-amd64.zip
 #echo -n "AuthToken"
-./ngrok authtoken xxxxxxxxxxxxxxxxxx
+./ngrok authtoken {AuthToken} 
 ./ngrok http 8080
