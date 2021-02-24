b16c768400cd2bb080c972ea36bae79a9970343a
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-02-24 23:17:32 +0530

    Update sk.sh

--- sk.sh
+++ sk.sh
@@ -13,6 +13,9 @@ rm -r *
 wget 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
 #unzipping ngrok-stable-linux-amd64.zip in selected folder
 unzip ngrok-stable-linux-amd64.zip
-#echo -n "AuthToken"
-./ngrok authtoken {AuthToken} 
+#asking ngrok auth token
+echo " Go to ngrok.io in any browser & signin or signup, copy the ngrok auth token and paste here, ngrok token only  (example - 4OXX56rxxxI00QGKnXXXXZ0_3xSAyW24irP0A0ie0bo0B),Readme - https://github.com/developeranaz/cloudshell-novnc-automation   Ngrok Auth token: "
+read input_token
+echo "You entered: $input_token"
+./ngrok authtoken $input_token
 ./ngrok http 8080
