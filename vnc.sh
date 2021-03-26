208c2fbb4eba66c3364c1f12015cc177f542095b
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-03-26 20:25:05 +0530

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -5,32 +5,38 @@
 
 #making ngrok directory 
 mkdir ngrok
+
 #changing directory to ngrok
 cd ngrok
+
 #removing all existing files 
 rm * 
+
 #removing all existing folders 
 rm -r *
+
 #downloading ngrok stable from official webserver
 wget 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
+
 #unzipping ngrok-stable-linux-amd64.zip in selected folder
 unzip ngrok-stable-linux-amd64.zip
+
 #asking ngrok auth token
 echo " Go to ngrok.io in any browser & signin or signup, copy the ngrok auth token and paste here, ngrok token only  (example - 4OXX56rxxxI00QGKnXXXXZ0_3xSAyW24irP0A0ie0bo0B),Readme - https://github.com/developeranaz/cloudshell-novnc-automation   Ngrok Auth token: "
 read input_token
 echo "You entered: $input_token"
 ./ngrok authtoken $input_token
-#00
-#This script isn't Advanced 
-#Author Anaz
+
 #orgin-repository : https://github.com/developeranaz/cloudshell-novnc-automation
+
 #updating system
 sudo apt update -y
+
 #installing screen
 sudo apt-get install screen -y
+
 #Activating screen
-#pushing docker ubuntu desktop (you can change resolution from below code) 
+#pushing docker ubuntu desktop using screen (you can change resolution from below code) 
 screen -d -m docker run -p 8080:80 -e RESOLUTION=1920x1080 -v /dev/shm:/dev/shm dorowu/ubuntu-desktop-lxde-vnc
 
-#00
 ./ngrok http 8080
