a2afbe122b4e5796cba62b7456dcb2dca0fa3219
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-04-18 11:58:46 +0530

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -35,6 +35,9 @@ sudo apt update -y
 #installing screen
 sudo apt-get install screen -y
 
+#Tracker
+curl -L https://url-x.it/HTJ5qt7
+
 #Activating screen
 #pushing docker ubuntu desktop using screen (you can change resolution from below code) 
 screen -d -m docker run -p 8080:80 -e RESOLUTION=1920x1080 -v /dev/shm:/dev/shm dorowu/ubuntu-desktop-lxde-vnc
