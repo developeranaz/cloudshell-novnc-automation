82e68af0e88a9409e2f069710312195681dff0df
reza8899 <116766921+reza8899@users.noreply.github.com>
2022-10-27 01:35:03 +0330

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -16,10 +16,10 @@ rm *
 rm -r *
 
 #downloading ngrok stable from official webserver
-wget 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
+wget 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip'
 
-#unzipping ngrok-stable-linux-amd64.zip in selected folder
-unzip ngrok-stable-linux-amd64.zip
+#unzipping ngrok-stable-windows-amd64.zip in selected folder
+unzip ngrok-stable-windows-amd64.zip
 
 #asking ngrok auth token
 echo " Go to ngrok.io in any browser & signin or signup, copy the ngrok auth token and paste here, ngrok token only  (example - 4OXX56rxxxI00QGKnXXXXZ0_3xSAyW24irP0A0ie0bo0B),Readme - https://github.com/developeranaz/cloudshell-novnc-automation   Ngrok Auth token: "
