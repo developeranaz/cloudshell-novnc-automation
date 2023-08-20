66dc5b85cebc6acdec2a985835fe52436af40e0f
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2023-08-20 07:41:53 +0530

    Update checkifalive.sh
    Pop message

--- checkifalive.sh
+++ checkifalive.sh
@@ -4,8 +4,8 @@ while true; do
     if curl -s http://localhost:8080 >/dev/null; then
         echo "Site is Online"
         echo "Go to "
-        curl http://localhost:4040/api/tunnels/ |sed 's/"/\n/g' |grep 'http:\|https:' |sort -u
-        
+        curl -s http://localhost:4040/api/tunnels/ |sed 's/"/\n/g' |grep 'http:\|https:' |sort -u
+        echo "Tunnel Successful. To restart or reload System Run `pkill screen; pkill ngrok; pkill supervisord; pkill containerd`"
         break
     else
         clear
